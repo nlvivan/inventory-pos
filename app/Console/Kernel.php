717fab6b427985731d63d54440adbe1e3f509352
Ivan <iantonio@netlinkvoice.com>
2024-11-27 12:12:57 +0800

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,16 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+
+        $schedule
+            ->command('app:notify-user-product-low-stock')
+            ->daily()
+            ->runInBackground();
+
+        $schedule
+            ->command('app:notify-user-for-expiry-date-command')
+            ->daily()
+            ->runInBackground();
     }
 
     /**
