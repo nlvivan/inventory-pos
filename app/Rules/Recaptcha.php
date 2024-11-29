16a169059bdbdae7c888434d3feec6a68911c262
Ivan <iantonio@netlinkvoice.com>
2024-11-29 10:24:20 +0800

    update

--- app/Rules/Recaptcha.php
+++ app/Rules/Recaptcha.php
@@ -15,12 +15,14 @@ class Recaptcha implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        $endpoint = config('services.google_recaptcha');
+        $endpoint = config('recaptcha.url');
 
-        $response = Http::asForm()->post($endpoint['url'], [
-            'secret' => $endpoint['secret_key'],
+        $response = Http::asForm()->post($endpoint, [
+            'secret' => config('recaptcha.secret_key'),
             'response' => $value,
         ])->json();
+
+        dd($response);
 
         if (! $response['success'] && ! $response['score'] > 0.5) {
             $fail('The captcha is incorrect.');
