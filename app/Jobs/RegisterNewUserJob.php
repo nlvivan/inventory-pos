ee37254199051ed03a35fcfd85fa1ab8c4056a55
Ivan <iantonio@netlinkvoice.com>
2025-05-20 06:17:25 +0800

    update

---
+++ app/Jobs/RegisterNewUserJob.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class RegisterNewUserJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(protected User $user)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        event(new Registered($this->user));
+    }
+}
