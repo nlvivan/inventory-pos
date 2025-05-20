498670d67d2e3badec43c12028545cc6f2c2c2f9
Ivan <iantonio@netlinkvoice.com>
2025-05-20 08:24:55 +0800

    defer

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,7 +46,7 @@ class UserController extends Controller
 
         $user->assignRole($request->role);
 
-        $user->notify(new \App\Notifications\SendCredsToUserNotification);
+        defer(fn () => $user->notify(new \App\Notifications\SendCredsToUserNotification));
 
         return redirect()->back();
     }
