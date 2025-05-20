2b7f999795dabd5fe63c516460a255174345f086
Ivan <iantonio@netlinkvoice.com>
2025-05-20 08:16:16 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\Rules;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/register', function (Request $request) {
+    $request->validate([
+        'first_name' => 'required|string|max:255',
+        'last_name' => 'required|string|max:255',
+        'middle_name' => 'nullable|string|max:255',
+        'email' => 'required|string|email|max:255|unique:'.User::class,
+        'password' => ['required', 'confirmed', Rules\Password::defaults()],
+    ]);
+
+    User::create([
+        'first_name' => $request->first_name,
+        'last_name' => $request->last_name,
+        'middle_name' => $request->middle_name,
+        'name' => $request->first_name.' '.$request->middle_name.' '.$request->last_name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+    ])->assignRole('customer');
 });
