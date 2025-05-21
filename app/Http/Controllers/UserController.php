81b41c3b0c3c529c3f510fdbaf8f6bba91a59a4b
Ivan <iantonio@netlinkvoice.com>
2025-05-21 13:12:17 +0800

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,7 +42,6 @@ class UserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make('password'),
-            'email_verified_at' => now(),
         ]);
 
         $user->assignRole($request->role);
